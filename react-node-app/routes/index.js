7f26c5934bf94021980d6d2ec3c2856eed8ca239
qiyu6 <qiyu6@illinois.edu>
2023-04-11 15:42:18 -0500

    edit app

--- react-node-app/routes/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (app, router, connection) {
-  app.use("/api", require("./home.js")(router, connection));
-  app.use("/api", require("./rso_page.js")(router, connection));
-  app.use("/api", require("./addRSO.js")(router, connection));
-};
