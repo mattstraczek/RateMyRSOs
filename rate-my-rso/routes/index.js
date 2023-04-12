5694df6af0aecf9da47812e092582552a7ccbfac
qiyu6 <qiyu6@illinois.edu>
2023-04-11 19:51:41 -0500

    upload midterm app

--- rate-my-rso/routes/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = function (app, router, connection) { 
-    app.use('/api', require('./home.js')(router, connection));
-    app.use('/api', require('./rso_page.js')(router, connection));
-};
